5675a0b1a18c385e33105c2b4a9bf7227d6a7905
meulk <51929618+meulk@users.noreply.github.com>
2021-05-03 17:46:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -85,11 +85,11 @@ sed -e 's|<|_|g; s|>|_|g; s|:|_|g; s|"|_|g; s|/|_|g; s|\\|_|g; s/|/_/g; s|?|_|g;
 #download (xmltv.sh) to filename $xmltvfilename - replace old time with new time
 xmltvfilename="${workdir}/${name}.xml"
 
-printf -- "\n${TICK} Downloading ${Green}xmltv.sh${COL_NC} from Github\n\n";
-wget -O ${installdir}/xmltv.sh "https://raw.githubusercontent.com/meulk/timeshift/main/xmltv.sh"
-sed -i "s|\\&|$url|" "${installdir}/xmltv.sh"
-sed -i "s|dddddd|$xmltvfilename|; s|ssssss|$source|" "${installdir}/xmltv.sh"
-chmod 755 ${installdir}/xmltv.sh
+printf -- "\n${TICK} Downloading ${Green}timeshift.sh${COL_NC} from Github\n\n";
+wget -O ${installdir}/timeshift.sh "https://raw.githubusercontent.com/meulk/timeshift/main/timeshift.sh"
+sed -i "s|\\&|$url|" "${installdir}/timeshift.sh"
+sed -i "s|dddddd|$xmltvfilename|; s|ssssss|$source|" "${installdir}/timeshift.sh"
+chmod 755 ${installdir}/timeshift.sh
 
 printf -- "\n${TICK} Downloading ${Green}uninstall.sh${COL_NC} from Github\n\n";
 wget -O ${installdir}/uninstall.sh "https://raw.githubusercontent.com/meulk/timeshift/main/uninstall.sh"
@@ -99,7 +99,7 @@ chmod 755 ${installdir}/uninstall.sh
 #stop cron error
 touch /etc/cron/crontabs/root
 #Add cronjob to run script at 8:15am
-crontab -l | { cat; echo "15 08 * * * /bin/sh /usr/script/xmltv.sh"; } | crontab -
+crontab -l | { cat; echo "15 08 * * * /bin/sh /usr/script/timeshift.sh"; } | crontab -
 printf -- "\n${TICK} Daily cron added to run script daily at 08:15am\n\n";
 sleep 2
 
@@ -114,7 +114,7 @@ printf -- "\n\nXML file HHMM is currently ${Red}$HHMM\n${COL_NC}";
 sleep 2
 
 if [ "$HHMM" = "+0000" ]; then
-   sh ${installdir}/xmltv.sh
+   sh ${installdir}/timeshift.sh
 
 else
 printf -- "\n${CROSS} ERROR: Script has been installed though it won't work until you follow one more step manually. Please make a note of following text\n";
