f176ba7a4f187e335c5adb282ec6006118287aee
meulk <51929618+meulk@users.noreply.github.com>
2021-05-04 10:23:57 +0100

    Create ppsetup.sh

---
+++ ppsetup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+#Install timeshift into ppanel 
+
+Green="\e[0;32m"  
+COL_NC="\e[0m"   
+TICK="[${Green}✓${COL_NC}]"
+
+ppaneldir="/var/etc/ppanels"
+
+#Create directories if they don't exist
+if [ ! -d "${ppaneldir}" ]; then
+  mkdir -p "${ppaneldir}" 
+fi
+
+printf -- "\n${TICK} Downloading ${Green}timeshift.xml${COL_NC} from Github\n";
+wget -O ${ppaneldir}/timeshift.xml "https://raw.githubusercontent.com/meulk/timeshift/main/timeshift.xml"
+
+exit 0;
